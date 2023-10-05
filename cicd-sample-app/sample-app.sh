f2422113e216cccd9b3cba78cdda1580f595d81c
DaanDeBonte <100585746+DaanDeBonte@users.noreply.github.com>
2023-10-05 16:17:17 +0200

    Update sample-app.sh

--- cicd-sample-app/sample-app.sh
+++ cicd-sample-app/sample-app.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 #mkdir tempdir
 #mkdir tempdir/templates
 #mkdir tempdir/static
-
+cd cicd-sample-app
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
