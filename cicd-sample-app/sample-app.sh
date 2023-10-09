6f427a7e058f0e57b48ab72f5f931860688c2fc0
DaanDeBonte <100585746+DaanDeBonte@users.noreply.github.com>
2023-10-09 20:26:05 +0200

    Update sample-app.sh

--- cicd-sample-app/sample-app.sh
+++ cicd-sample-app/sample-app.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-# Create tempdir if it doesn't exist
-if [ ! -d "tempdir" ]; then
-    mkdir tempdir
-fi
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
 
-# Create subdirectories
-mkdir -p tempdir/templates
-mkdir -p tempdir/static
-
-
-cd cicd-sample-app
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
